f32edf91ea77ab6b2d520bff33d55e1c1a852b6f
Lokesh Kumawat <Lokeshkumawat5590@gmailcom>
2025-06-25 17:07:05 +0530

    database file update

--- DB.js
+++ DB.js
@@ -6,16 +6,19 @@ let isConnected = false;
 export const connectDB = async () => {
     if (isConnected) return;
 
+    // not nacessary
+    if(!process.env.DATABASE_URL){
+        console.log("database url not in env file");
+        throw new Error("missing mongodb string")
+    }
+
     try {
-        await mongoose.connect(process.env.DATABASE_URL//, {
-            //     useNewUrlParser: true,
-            //     useUnifiedTopology: true,
-            // }
-        );
+        await mongoose.connect(process.env.DATABASE_URL);
         isConnected = true;
         console.log("MongoDB connected");
     } catch (err) {
         console.error("DB connection error:", err.message);
+        throw err;
     }
 
 };
